2d8adacc4eed4135d308f813b306aac83234a4ca
Daiana <edinoroshechka@yandex.com>
2024-12-21 15:50:04 +0300

    add loadCards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+let templateCard = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+let cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(name, urlImage) {
+
+    let card = templateCard.querySelector(".card").cloneNode(true);
+
+    card.querySelector(".card__image").src = urlImage;
+    card.querySelector(".card__image").alt = name;
+    card.querySelector(".card__title").textContent = name;
+
+    card.querySelector(".card__delete-button").addEventListener("click", deleteCard);
+
+    cardList.append(card);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    event.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function loadCards(infoCards) {
+    for (let item of infoCards) {
+        createCard(item.name, item.link);
+    }
+}
+
+loadCards(initialCards);
