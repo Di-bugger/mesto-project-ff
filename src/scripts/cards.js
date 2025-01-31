e275541d14c75787367a6fc6fe512020e9d7a25e
Daiana <edinoroshechka@yandex.com>
2025-01-31 17:04:12 +0300

    add transition slowly popup

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,3 +1,5 @@
+import {closeModal, openModal} from "./modal";
+
 export const initialCards = [
     {
       name: "Архыз",
@@ -33,8 +35,12 @@ function deleteCard(event) {
     event.target.parentElement.remove();
 }
 
+function handleLikeCard(card) {
+    card.querySelector('.card__like-button').classList.toggle('card__like-button_is-active')
+}
+
 // @todo: Функция создания карточки
-export function createCard(name, urlImage) {
+export function createCard(name, urlImage, likeAction, imgPopup) {
 
     const card = templateCard.querySelector(".card").cloneNode(true);
     const cardImage = card.querySelector(".card__image");
@@ -43,6 +49,14 @@ export function createCard(name, urlImage) {
     card.querySelector(".card__title").textContent = name;
 
     card.querySelector(".card__delete-button").addEventListener("click", deleteCard);
+    card.querySelector(".card__like-button").addEventListener("click", () => {
+        likeAction(card);
+    });
+    cardImage.addEventListener("click", () => {
+        imgPopup(name, urlImage)
+    })
 
     return card;
 }
+
+export {handleLikeCard};
